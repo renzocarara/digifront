e3d37276f98ce3569cb1808ea3d851b3b8683d4a
Renzo Carara <renzo.carara@libero.it>
2020-07-20 16:58:04 +0200

    inserito un link

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,7 +9,7 @@
       <li><strong>Read</strong>: è possibile accedere alle informazioni salvate nel DB</li>
       </ul>
       <br>
-      <p>Le operazioni sul DB vengono effettuate tramite chiamate API, che permettono di leggere e scrivere su un DB PostgreSQL. Il DB e le API esposte, fanno parte di un'applicazione BACKEND (DigiBack), deployata su HEROKU (https://digiback.herokuapp.com/). <br>
+      <p>Le operazioni sul DB vengono effettuate tramite chiamate API, che permettono di leggere e scrivere su un DB PostgreSQL. Il DB e le API esposte, fanno parte di un'applicazione BACKEND (<a target="_blank" href="https://digiback.herokuapp.com">DigiBack</a>), deployata su HEROKU. <br>
          L'applicazione BACKEND è scritta in Laravel + Bootstrap. <br>
          L'applicazione FRONTEND è scritta in Vue ed utilizza Vue-router, Vuex e Vuetify<br>
       </p>
